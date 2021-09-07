2f590d5e1c54dd0d2d35a6969bb1cbeb63654f96
vedikaag78 <vedika1510@gmail.com>
2021-09-07 13:56:18 +0530

    Setup

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+ <div id="app">
+   <Navigation />
+    <router-view/>
+ </div>
+  
+</template>
+
+<script>
+
+import Navigation from "./components/Navigation.vue";
+export default {
+  name: "App",
+  components: {
+    
+    Navigation
+  },
+};
+</script>
+
+<style lang="scss">
+@import "node_modules/bootstrap/scss/bootstrap";
+</style>
